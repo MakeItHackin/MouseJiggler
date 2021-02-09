3f71dd52604e7e689bbc8110e426f73ff78f21e9
MakeItHackin <projects@makeithackin.com>
2021-02-09 13:41:34 -0600

    Update MouseJigglerPowershell.ps1

--- MouseJigglerPowershell.ps1
+++ MouseJigglerPowershell.ps1
@@ -1,2 +1,17 @@
-#test 
 
+# MouseJiggler Powershell Script
+# Written by AndrewDavis 
+# https://gist.github.com/AndrewDavis
+
+Clear-Host
+Echo "Keep-alive with Scroll Lock..."
+
+$WShell = New-Object -com "Wscript.Shell"
+
+while ($true)
+{
+  $WShell.sendkeys("{SCROLLLOCK}")
+  Start-Sleep -Milliseconds 100
+  $WShell.sendkeys("{SCROLLLOCK}")
+  Start-Sleep -Seconds 240
+}
